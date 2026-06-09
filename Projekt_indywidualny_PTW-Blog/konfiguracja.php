55755c8aabe55878d431813dd7fb75e05c17c42d
szu23 <139860967+szu23@users.noreply.github.com>
2026-06-09 10:28:16 +0200

    Delete Projekt_indywidualny_PTW-Blog directory

--- Projekt_indywidualny_PTW-Blog/konfiguracja.php
+++
@@ -1,6 +0,0 @@
-<?php
-$polaczenie = new mysqli('localhost', 'root', '', 'blog');
-
-if($polaczenie->connect_error){
-    die("ERROR: Could not connect. " . $polaczenie->connect_error);
-}
--- Projekt_indywidualny_PTW-Blog/sesja.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
- 
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
-}
-require_once "konfiguracja.php";
